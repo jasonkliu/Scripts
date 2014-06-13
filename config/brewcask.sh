c4eec6ed124a6e36561b8d1cd81d1986d04f6b97
Jason Liu <jason@jasonkliu.com>
2014-06-12 21:03:06 -0700

    Add brew cask and brew files list

---
+++ config/brewcask.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Install homebrew-cask (assumes brew already installed)
+# brew install caskroom/cask/brew/cask
+
+# Manually install:
+# Sublime Text 3, TeX, XCode, iTerm2, CS5, ADT, XCode
+# Android Studio, Cisco VPN
+
+brew cask install alfred atom dropbox flux macvim mou 
+brew cask install pckeyboardhack skype spotify thunderbird 
+brew cask install transmission virtualbox
+brew cask install android-file-transfer
+
+# rbenv install 2.1.2 && rbenv global 2.1.2
+brew install rbenv ruby-build
+brew install bash fish
+brew install fortune cowsay figlet
+brew install git vim wget
+brew install go rust python python3
