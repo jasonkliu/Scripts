5f74f4f52e91b0166b833910414983b1129ef548
Jason Liu <jason@jasonkliu.com>
2014-06-13 22:45:13 -0700

    Hide Dashboard as a space

--- config/osx.sh
+++ config/osx.sh
@@ -6,4 +6,9 @@ defaults write com.apple.dock showhidden -bool YES
 # Disable Notification Center and remove the menu bar icon
 launchctl unload -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist 2> /dev/null
 
+# Disable Dashboard
+defaults write com.apple.dashboard mcx-disabled -bool true
+
+# Don’t show Dashboard as a Space
+defaults write com.apple.dock dashboard-in-overlay -bool true
 
